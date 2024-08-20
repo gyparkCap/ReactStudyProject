581a32231e60b77085aacf6edeb776b04edda4ea
gy.park <gy.park@waldlust.co.kr>
2024-08-20 13:20:00 +0900

    create-react-app

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+/*
+함수 선언시 아래 3가지 중 아무거나 택해서 사용하면 됨
+*/
+// const Hello = function () {
+
+// };
+
+// const Hello = () => {
+
+// };
+
+export default function Hello(){
+    return <p>Hello</p>;
+}
