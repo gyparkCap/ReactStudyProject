06a4118c6b540325ed5804d6e62ef802f2ac9afa
gy.park <gy.park@waldlust.co.kr>
2024-08-20 14:08:20 +0900

    라우팅 테스트

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Hello from './components/Hello';
-import Test from './components/test';
-
+import Test from './components/test'; // 'test' 대신 'Test'로 수정
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Switch를 Routes로 변경
 
 function App() {
-  return (
-      <div className="App">
-        <Hello />
-
-        <Test />
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes> {/* Switch 대신 Routes 사용 */}
+                    <Route path="/" element={<Hello />} /> {/* component 대신 element prop 사용 */}
+                    <Route path="/day" element={<Test />} /> {/* 동일하게 element prop 사용 */}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
